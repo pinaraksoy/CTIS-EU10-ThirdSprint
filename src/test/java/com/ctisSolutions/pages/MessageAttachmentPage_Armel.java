00d429996a5517b54f5532f3faad5e93e8417d16
Armelinda Morina Wijaya <armelindamorina@gmail.com>
2023-04-18 00:25:00 +0300

    message attachment page : web element locator added

--- src/test/java/com/ctisSolutions/pages/MessageAttachmentPage_Armel.java
+++ src/test/java/com/ctisSolutions/pages/MessageAttachmentPage_Armel.java
@@ -24,16 +24,16 @@ public class MessageAttachmentPage_Armel {
 
 
     //TS02
-    @FindBy (xpath = "")
+    @FindBy (xpath = "//div[@id='feed-add-post-destination-container']")
     public WebElement addLink_linkIcon;
 
-    @FindBy (xpath = "")
+    @FindBy (xpath = "//input[@id='linkidPostFormLHE_blogPostForm-text']")
     public WebElement addLink_textField;
 
-    @FindBy (xpath = "")
+    @FindBy (xpath = "//input[@id='linkidPostFormLHE_blogPostForm-href']")
     public WebElement addLink_linkField;
 
-    @FindBy (xpath = "")
+    @FindBy (xpath = "//input[@id='']")
     public WebElement addLink_saveButton;
 
     @FindBy (xpath = "")
@@ -41,16 +41,16 @@ public class MessageAttachmentPage_Armel {
 
     //TS03
 
-    @FindBy (xpath = "")
+    @FindBy (xpath = "//span[@title='Insert video']//i")
     public WebElement insertVideo_insertVideoIcon;
 
-    @FindBy (xpath = "")
+    @FindBy (xpath = "//input[@id='video_idPostFormLHE_blogPostForm-source']")
     public WebElement insertVideo_videoSource;
 
-    @FindBy (xpath = "")
+    @FindBy (xpath = "//div[@class='bx-core-adm-dialog-content-wrap adm-workarea bxhtmled-dialog bxhtmled-video-dialog']//input[@id='']")
     public WebElement insertVideo_saveButton;
 
-    @FindBy (xpath = "")
+    @FindBy (xpath = "//span[@class='bxhtmled-video-error']")
     public WebElement insertVideo_allertMessage;
 
     //TS04
@@ -65,17 +65,17 @@ public class MessageAttachmentPage_Armel {
 
     //TS05
 
-    @FindBy (xpath = "")
+    @FindBy (xpath = "//span[@id='bx-b-tag-input-blogPostForm']")
     public WebElement addTag_tagIcon;
 
-    @FindBy (xpath = "")
+    @FindBy (xpath = "//div[@id='post-tags-container-blogPostForm']")
     public WebElement addTag_tagField;
 
-    @FindBy (xpath = "")
+    @FindBy (xpath = "//span[@class='popup-window-button']")
     public WebElement addTag_addButton;
 
     //TS06
-    @FindBy (xpath = "")
+    @FindBy (xpath = "//span[@class='feed-add-post-tags']")
     public WebElement addTag_addedTag;
 
 
@@ -89,7 +89,7 @@ public class MessageAttachmentPage_Armel {
 
     //end step
 
-    @FindBy (xpath = "")
+    @FindBy (xpath = "//button[@id='blog-submit-button-save']")
     public WebElement sendButton;
 
 
