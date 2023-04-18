4523dd4d10b009a5c4ac4ab2a550c162612622ae
erdalkalkan <83866679+erdalkalkan@users.noreply.github.com>
2023-04-19 00:47:35 +0200

    final senario

--- src/test/java/com/ctisSolutions/runners/CukesRunner.java
+++ src/test/java/com/ctisSolutions/runners/CukesRunner.java
@@ -16,7 +16,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/ctisSolutions/step_definitions",
         dryRun = false,
-        tags = "@wip",
+        tags = "@CRM",
         publish = false
 
 )
